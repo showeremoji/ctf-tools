7083f2f2a621b2bac8cd878ad403c754f906a6d1
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-05-09 21:00:46 +0200

    More basics

--- py-snippets.py
+++ py-snippets.py
@@ -1,1 +1,10 @@
-print("for printing")
+#!/usr/bin/env python2
+import base64
+
+# Standard form is a hex string without 0x at the start
+
+def b642hex(b):
+    return base64.b64decode('woidjw==').encode('hex')
+
+def hex2b64(h):
+    return poop
