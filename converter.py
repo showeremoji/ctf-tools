18b3bdae1af03449e987171fdade97903b12c6d3
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-05-09 21:36:01 +0200

    Add binary string conversion

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 import base64
+import binascii
 
 # Standard form is a hex string without 0x at the start
 
@@ -9,7 +10,17 @@ def b642hex(b):
 def hex2b64(h):
     return h.decode("hex").encode("base64").strip()
 
+def bin2hex(b):
+    return hex(int(b, 2))[2:]
+
+def hex2bin(h):
+    return bin(int(h, 16))[2:]
+
 if __name__ == '__main__':
     assert(b642hex('woidjw==') == 'c2889d8f')
     assert(hex2b64('c2889d8f') == 'woidjw==')
+
+    assert(bin2hex('1100000011011110') == 'c0de')
+    assert(hex2bin('c0de') == '1100000011011110')
+
     print('Asserts passed')
